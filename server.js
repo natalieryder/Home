fa507f6b57cd0c1db0911738974c07da90b3466a
Amir Nasehi <nasehi.amir@gmail.com>
2018-05-21 18:39:37 +0430

    server added

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const session = require('express-session');
+const mongoose = require("mongoose");
+const MongoStore = require('connect-mongo')(session);
+const server = require('http').createServer(app);
+
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const {MONGODB_URI} = require('./config/');
+require('./libs/db-connection');
+
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'/public')));
+app.use(require('./routes/')); // main routes
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+
+  server.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
